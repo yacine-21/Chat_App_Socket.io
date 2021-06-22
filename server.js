d5b4e413bb9b54d6a8ec7cf97939deb5c4eb3cf7
Yacine Lyoubi <yacinelyoubi37@gmail.com>
2021-06-22 16:09:58 +0200

    add connection real time with socket io

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
-const port = process.env.PORT || 3000
+const socketIO = require("socket.io");
+
 const publicPath = path.join(__dirname,"./Public");
+const port = process.env.PORT || 3000
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, ()=>{
+io.on("connection",(socket)=>{
+    console.log("A new user just connected");
+
+    socket.on("disconnect",()=>{
+        console.log("User was disconnected.");
+    });
+});
+
+
+
+
+server.listen(port, ()=>{
     console.log(`Server is on port ${port}`);
 })
